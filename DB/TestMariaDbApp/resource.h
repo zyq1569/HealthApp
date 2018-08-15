5e357c862ec0ae6274415fe2dfdd425f73e5b54d
zyq1569 <zyq1569@126.com>
2018-08-15 16:06:29 +0800

    修改mysql的查询内容

--- DB/TestMariaDbApp/resource.h
+++ DB/TestMariaDbApp/resource.h
@@ -9,6 +9,13 @@
 #define IDR_MAINFRAME                   128
 #define IDC_BUTTON1                     1000
 #define IDC_BUTTON2                     1001
+#define IDC_IP                          1002
+#define IDC_NAME                        1003
+#define IDC_PWD                         1004
+#define IDC_LOG                         1005
+#define IDC_SQL                         1006
+#define IDC_PWD2                        1007
+#define IDC_DATANAME                    1007
 
 // Next default values for new objects
 // 
@@ -16,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
