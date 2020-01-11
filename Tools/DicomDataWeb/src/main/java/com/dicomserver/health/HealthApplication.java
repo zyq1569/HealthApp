ca48dbd9d2fd7f8749413912f7a86085b53644c6
zyq1569 <zyq1569@126.com>
2020-01-11 18:25:13 +0800

    modify LogManager

--- Tools/DicomDataWeb/src/main/java/com/dicomserver/health/HealthApplication.java
+++ Tools/DicomDataWeb/src/main/java/com/dicomserver/health/HealthApplication.java
@@ -15,7 +15,12 @@ import io.netty.handler.ssl.SslProvider;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 //	public static void main(String[] args) {
 //		SpringApplication.run(HealthApplication.class, args);
@@ -26,23 +31,31 @@ public class HealthApplication {
     static final boolean SSL = System.getProperty("ssl") != null;
     //	static final int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8443" : "8080"));
     private static final ServerConfig serverconfig = new ServerConfig();
+
     public static void main(String[] args) throws Exception {
+//        LoggerContext logContext = LogManager.getContext(false);
+        LoggerContext logContext = (LoggerContext) LogManager.getContext(false);
+        File conFile = new File("D:/code/C++/HealthApp/bin/win32/log4j2.xml");
+        logContext.setConfigLocation(conFile.toURI());
+        logContext.reconfigure();
         Logger log = LogManager.getLogger(HealthApplication.class);
+//        D:\code\C++\HealthApp\bin\win32
+//        log.info(HealthApplication.getAttribute("log4j2ConfigFile"));
         if (args.length == 7) {
             serverconfig.bparm = true;
             serverconfig.driver = args[0];
-			serverconfig.url = args[1];
-			serverconfig.username = args[2];
-			serverconfig.password = args[3];
-			serverconfig.port = args[4];
-			serverconfig.filePath = args[5];
-			serverconfig.webdir = args[6];
-			String port = serverconfig.getString("port");
+            serverconfig.url = args[1];
+            serverconfig.username = args[2];
+            serverconfig.password = args[3];
+            serverconfig.port = args[4];
+            serverconfig.filePath = args[5];
+            serverconfig.webdir = args[6];
+            String port = serverconfig.getString("port");
             System.err.println(port);
-            log.info("HealthApplication string:{}",port);
-			for (String s:args){
-                log.info("args string:{}",s);
-			}
+            log.info("HealthApplication string:{}", port);
+            for (String s : args) {
+                log.info("args string:{}", s);
+            }
         }
         //////////////--------------------------
         final SslContext sslCtx;
@@ -63,7 +76,7 @@ public class HealthApplication {
                     .childHandler(new HttpStaticFileServerInitializer(sslCtx));
             Channel ch = b.bind(Integer.parseInt(serverconfig.getString("port")/*PORT*/)).sync().channel();
 //			Channel ch = b.bind(PORT).sync().channel();
-            log.info("HealthApplication :{}","Open your web browser and navigate to " + (SSL ? "https" : "http") + "://127.0.0.1:" + serverconfig.getString("port") + '/');
+            log.info("HealthApplication :{}", "Open your web browser and navigate to " + (SSL ? "https" : "http") + "://127.0.0.1:" + serverconfig.getString("port") + '/');
             ch.closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
