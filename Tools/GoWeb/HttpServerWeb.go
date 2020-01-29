b1fcadd0d3648f1a8ff04e78dc3ec801661f9572
zyq1569 <zyq1569@126.com>
2020-01-29 21:35:56 +0800

    add go code web

--- Tools/GoWeb/HttpServerWeb.go
+++ Tools/GoWeb/HttpServerWeb.go
@@ -22,10 +22,10 @@ func Login(c echo.Context) error {
 	req := c.Request()
 	println("req.URL.Path:" + req.URL.Path)
 	println("D:/code/C++/HealthApp/Tools/PageWeb" + req.URL.Path)
-
 	filepath := "D:/code/C++/HealthApp/Tools/PageWeb" + req.URL.Path
 	return c.File(filepath)
 }
+
 func checkErr(err error) {
 	if err != nil {
 		panic(err)
@@ -36,30 +36,22 @@ func checkMariDB() {
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/hit?charset=utf8")
 	rows, err := db.Query("SELECT * FROM h_user")
 	for rows.Next() {
-		var uid int
+		var id int
 		var username string
-
-		err = rows.Scan(&uid, &username)
+		err = rows.Scan(&id, &username)
 		checkErr(err)
-		fmt.Println(uid)
+		fmt.Println(id)
 		fmt.Println(username)
 	}
 }
+
 func main() {
+	checkMariDB()
 	flag.Parse() //暂停获取参数
 	e := echo.New()
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${time_rfc3339_nano} ${remote_ip} ${method} ${uri} ${status}\n",
 	}))
 	e.GET("/Login/*", Login)
-	// e.GET("/Login/*", func(c echo.Context) error {
-	// 	req := c.Request()
-	// 	println("req.URL.Path:" + req.URL.Path)
-	// 	println("D:/code/C++/HealthApp/Tools/PageWeb" + req.URL.Path)
-
-	// 	filepath := "D:/code/C++/HealthApp/Tools/PageWeb" + req.URL.Path
-	// 	return c.File(filepath)
-
-	// })
 	e.Logger.Fatal(e.Start(":9090"))
 }
