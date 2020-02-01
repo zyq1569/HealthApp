5e455e3db025a083ee4f0dcd73bf02f84e28b19e
zyq1569 <zyq1569@126.com>
2020-02-01 21:33:33 +0800

    add:  reportdata struct file

---
+++ Tools/GoWeb/Data/ReportData.go
@@ -0,0 +1,16 @@
+package Report
+
+type ReportData struct {
+	ReportIdentity     string
+	StudyOrderIdentity string
+	ReportTemplate     string
+	ReportCreatDate    string
+	ReportWriterID     string
+	ReportCheckID      string
+	ReportCheckDate    string
+	ReportContent      string
+	ReportState        string
+	ReportSave         string
+	ReportChange       string
+	ReportOther        string
+}
