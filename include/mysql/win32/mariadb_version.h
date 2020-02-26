d35a81e09727b9b16901a08bcdf67ba81f7960a9
zyq1569 <zyq1569@126.com>
2020-02-26 20:46:21 +0800

    add mariadb.lib

--- include/mysql/win32/mariadb_version.h
+++ include/mysql/win32/mariadb_version.h
@@ -9,30 +9,30 @@
 #ifdef _CUSTOMCONFIG_
 #include <custom_conf.h>
 #else
-#define PROTOCOL_VERSION		@PROTOCOL_VERSION@
-#define MARIADB_CLIENT_VERSION_STR	"@MARIADB_CLIENT_VERSION@"
-#define MARIADB_BASE_VERSION		"@MARIADB_BASE_VERSION@"
-#define MARIADB_VERSION_ID		@MARIADB_VERSION_ID@
-#define MARIADB_PORT	        	@MARIADB_PORT@
-#define MARIADB_UNIX_ADDR               "@MARIADB_UNIX_ADDR@"
+#define PROTOCOL_VERSION		10
+#define MARIADB_CLIENT_VERSION_STR	"10.4.3"
+#define MARIADB_BASE_VERSION		"mariadb-10.4"
+#define MARIADB_VERSION_ID		100403
+#define MARIADB_PORT	        	3306
+#define MARIADB_UNIX_ADDR               "/tmp/mysql.sock"
 
 #define MYSQL_CONFIG_NAME               "my"
-#define MYSQL_VERSION_ID                @MARIADB_VERSION_ID@
-#define MYSQL_SERVER_VERSION            "@MARIADB_CLIENT_VERSION@-MariaDB"
+#define MYSQL_VERSION_ID                100403
+#define MYSQL_SERVER_VERSION            "10.4.3-MariaDB"
 
-#define MARIADB_PACKAGE_VERSION "@CPACK_PACKAGE_VERSION@"
-#define MARIADB_PACKAGE_VERSION_ID @MARIADB_PACKAGE_VERSION_ID@
-#define MARIADB_SYSTEM_TYPE "@CMAKE_SYSTEM_NAME@"
-#define MARIADB_MACHINE_TYPE "@CMAKE_SYSTEM_PROCESSOR@"
-#define MARIADB_PLUGINDIR "@CMAKE_INSTALL_PREFIX@/@INSTALL_PLUGINDIR@"
+#define MARIADB_PACKAGE_VERSION "3.1.7"
+#define MARIADB_PACKAGE_VERSION_ID 30107
+#define MARIADB_SYSTEM_TYPE "Windows"
+#define MARIADB_MACHINE_TYPE "AMD64"
+#define MARIADB_PLUGINDIR "C:/Program Files (x86)/mariadb-connector-c/lib/mariadb/plugin"
 
 /* mysqld compile time options */
 #ifndef MYSQL_CHARSET
-#define MYSQL_CHARSET			"@default_charset@"
+#define MYSQL_CHARSET			""
 #endif
 #endif
 
 /* Source information */
-#define CC_SOURCE_REVISION "@CC_SOURCE_REVISION@"
+#define CC_SOURCE_REVISION ""
 
 #endif /* _mariadb_version_h_ */
