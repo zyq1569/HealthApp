a22d9ba0c5ca024f13161dea3a7cc56c50885280
zyq1569 <zyq1569@126.com>
2020-03-02 13:24:40 +0800

    adjust code

--- DB/libmariadb/codeblock/cmake_install.cmake
+++ DB/libmariadb/codeblock/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/mariadb-connector-c")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/mariadb")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
