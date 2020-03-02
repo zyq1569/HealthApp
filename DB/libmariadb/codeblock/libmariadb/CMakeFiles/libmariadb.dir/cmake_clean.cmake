9934386857778fde84f2dcbbdee53f57d9cfb245
zyq1569 <zyq1569@126.com>
2020-03-02 13:27:56 +0800

    adjust code

---
+++ DB/libmariadb/codeblock/libmariadb/CMakeFiles/libmariadb.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libmariadb.dir/__/win/libmariadb.rc.obj"
+  "liblibmariadb.dll.a"
+  "libmariadb.dll"
+  "libmariadb.dll.manifest"
+  "libmariadb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang RC)
+  include(CMakeFiles/libmariadb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
