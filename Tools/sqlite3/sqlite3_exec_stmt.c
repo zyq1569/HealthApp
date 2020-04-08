696bbb45435ded594eeec1c981899cf750160a50
zyq1569 <zyq1569@126.com>
2020-04-08 12:44:11 +0800

    add fmjpeg2k

--- Tools/sqlite3/sqlite3_exec_stmt.c
+++ Tools/sqlite3/sqlite3_exec_stmt.c
@@ -21,69 +21,69 @@ http://opensource.org/licenses/MIT
 ** is invoked, even for queries.
 */
 int sqlite3_exec_stmt(
-	sqlite3_stmt *pStmt,           /* The SQL to be executed */
-	sqlite3_callback xCallback, /* Invoke this callback routine */
-	void *pArg,                 /* First argument to xCallback() */
-	char **pzErrMsg             /* Write error messages here */
-	){
-		int rc = SQLITE_OK;
-		sqlite3 *db = 0;
-		char **azCols = 0;
+    sqlite3_stmt *pStmt,           /* The SQL to be executed */
+    sqlite3_callback xCallback, /* Invoke this callback routine */
+    void *pArg,                 /* First argument to xCallback() */
+    char **pzErrMsg             /* Write error messages here */
+    ){
+        int rc = SQLITE_OK;
+        sqlite3 *db = 0;
+        char **azCols = 0;
 
-		int nRetry = 0;
-		int nChange = 0;
-		int nCallback;
+        int nRetry = 0;
+        int nChange = 0;
+        int nCallback;
 
-		if( pStmt ==0 ) return SQLITE_OK;
-		db = sqlite3_db_handle(pStmt);
-		while( (rc==SQLITE_OK || (rc==SQLITE_SCHEMA && (++nRetry)<2))){
-			int nCol;
-			char **azVals = 0;
+        if( pStmt ==0 ) return SQLITE_OK;
+        db = sqlite3_db_handle(pStmt);
+        while( (rc==SQLITE_OK || (rc==SQLITE_SCHEMA && (++nRetry)<2))){
+            int nCol;
+            char **azVals = 0;
 
-			nCallback = 0;
+            nCallback = 0;
 
-			nCol = sqlite3_column_count(pStmt);
-			azCols = (char **) malloc(2*nCol*sizeof(const char *) + 1);
-			if( azCols==0 ){
-				goto exec_out;
-			} 
+            nCol = sqlite3_column_count(pStmt);
+            azCols = (char **) malloc(2*nCol*sizeof(const char *) + 1);
+            if( azCols==0 ){
+                goto exec_out;
+            } 
 
-			while( 1 ){
-				int i;
-				rc = sqlite3_step(pStmt);
+            while( 1 ){
+                int i;
+                rc = sqlite3_step(pStmt);
 
-				/* Invoke the callback function if required */
-				if( xCallback && (SQLITE_ROW==rc ||
-					(SQLITE_DONE==rc && !nCallback))
-					){
-						if( 0==nCallback ){
-							for(i=0; i<nCol; i++){
-								azCols[i] = (char *)sqlite3_column_name(pStmt, i);
-							}
-							nCallback++;
-						}
-						if( rc==SQLITE_ROW ){
-							azVals = &azCols[nCol];
-							for(i=0; i<nCol; i++){
-								azVals[i] = (char *)sqlite3_column_text(pStmt, i);
-							}
+                /* Invoke the callback function if required */
+                if( xCallback && (SQLITE_ROW==rc ||
+                    (SQLITE_DONE==rc && !nCallback))
+                    ){
+                        if( 0==nCallback ){
+                            for(i=0; i<nCol; i++){
+                                azCols[i] = (char *)sqlite3_column_name(pStmt, i);
+                            }
+                            nCallback++;
+                        }
+                        if( rc==SQLITE_ROW ){
+                            azVals = &azCols[nCol];
+                            for(i=0; i<nCol; i++){
+                                azVals[i] = (char *)sqlite3_column_text(pStmt, i);
+                            }
 
-							if( xCallback(pArg, nCol, azVals, azCols) ){
-								rc = SQLITE_ABORT;
-								goto exec_out;
-							}
-						}
-				}
+                            if( xCallback(pArg, nCol, azVals, azCols) ){
+                                rc = SQLITE_ABORT;
+                                goto exec_out;
+                            }
+                        }
+                }
 
-				if( rc!=SQLITE_ROW ){                   
-					break;
-				}
-			}
-		}
+                if( rc!=SQLITE_ROW ){                   
+                    break;
+                }
+            }
+        }
 
 exec_out:
-		if( azCols ) free(azCols); 
-		sqlite3_reset(pStmt);
+        if( azCols ) free(azCols); 
+        sqlite3_reset(pStmt);
 
-		return rc;
+        return rc;
 }
