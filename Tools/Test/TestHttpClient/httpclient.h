9a8ec6e4a3a75fb52b3a63c82ed8165d22c45c10
zyq1569 <zyq1569@126.com>
2020-10-17 23:02:06 +0800

    HttpClient

--- Tools/Test/TestHttpClient/httpclient.h
+++ Tools/Test/TestHttpClient/httpclient.h
@@ -6,7 +6,7 @@
 #include <QUrl>
 #include <QSslError>
 #include <QNetworkAccessManager>
-
+#include <QList>
 
 class QNetworkReply;
 class QAuthenticator;
@@ -25,12 +25,90 @@ public slots:
     void networkReplyProgress(qint64 bytesRead, qint64 totalBytes);
 };
 
-enum DownFileType {
+///-----------------------------------------------------------------------------------------------------------------------
+enum DownFileType
+{
     dcm,
-    json,
+    dbinfo,
+    studyini,
     other
 };
 
+typedef  QString  OFString;
+struct DicomFileInfo
+{
+    OFString patientName;
+    OFString patientSex;
+    OFString patientAge;
+    OFString patientBirthDate;
+    OFString patientBirthTime;
+    OFString patientId;
+
+    OFString modality;
+    OFString manufacturer;
+    OFString institutionName;
+
+    OFString studyId;
+    OFString studyUID;
+    OFString studyDescription;
+    OFString studyDate;
+    OFString studyTime;
+
+    OFString seriesUID;
+    OFString seriesDescription;
+    OFString seriesNumber;
+
+    OFString imageSOPInstanceUID;
+    OFString instanceNumber;
+};
+
+struct ImageInfo
+{
+    //(0008,0018) UI 1.2.840.113619.2.55.3.604688119.699.1256270047.398.2   # 1, 52 SOP Instance UID
+    OFString imageSOPInstanceUID;
+    // (0020, 0013) IS 2                                                 # 1, 2 Instance Number;
+    int instanceNumber;
+};
+
+struct SeriesInfo
+{
+    QList<ImageInfo> imagesInfoList;
+    OFString seriesUID;
+    OFString seriesDescription;
+    //Series Number
+    int seriesNumber;
+};
+
+struct StudyInfo
+{
+    OFString patientName;
+    OFString patientId;
+    OFString modality;
+    OFString studyId;
+    OFString studyUID;
+    OFString studyDate;
+    OFString studyDescription;
+    QList<SeriesInfo> seriesInfoList;
+};
+
+///----------------------------------------------------------------------------------------
+struct StudyRowInfo
+{
+    QString patientIdentity,patientName,patientId,patientSex,patientBirthday,patientTelNumber;
+    QString patientAddr,patientCarID,patientType,patientEmail,studyOrderIdentity,studyId,studyuid;
+    QString scheduledDateTime,ScheduledDate,orderDateTime,studyDescription,studyModality,aETitle;
+    QString studyType,studyCode,studyState,studyCost,studyDate,studyDepart,sStudyModality,costType;
+};
+
+struct PatientStudyDB
+{
+    int code;
+    QString msg,ver;
+    int count;
+    QList<StudyRowInfo> rowinfo;
+};
+
+///----------------------------------------------------------------------------------------
 class HttpClient : public QObject
 {
     Q_OBJECT
@@ -39,10 +117,10 @@ public:
 
 
 public:
+    void setPatientDBinfo(QJsonValue &JsonValue,StudyRowInfo &Rowinfo);
     void setDwonloadDir(QString dir);
     void getStudyDBinfo(QUrl url,QString start,QString end,QString page,QString limit);//start=19700101&end=20191230&page=1&limit=10
     void getStudyImageFile(QUrl url,QString studyuid="",QString seruid = "", QString imguid = "");
-
 signals:
 
 public slots:
@@ -60,6 +138,7 @@ private:
     bool openFileForWrite(const QString &fileName);
     void downFileFromWeb(QUrl httpUrl,QString savefilename,QString downDir);
     void startRequest(const QUrl &requestedUrl);
+    void ParseDwonData();
 private:
     QUrl m_url;
     QNetworkAccessManager m_networkmanager;
@@ -69,6 +148,7 @@ private:
     QString m_downDir;
     DownFileType m_currentfiletype;
     QByteArray m_currentDownData;
+    PatientStudyDB m_patientstudydb;
 
 };
 
