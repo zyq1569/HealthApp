618be8b5ff2fed6c4c8cec514afbbc4865b0e429
zyq1569 <zyq1569@126.com>
2020-10-17 23:02:06 +0800

    HttpClient

--- Tools/Test/TestHttpClient/httpclient.cpp
+++ Tools/Test/TestHttpClient/httpclient.cpp
@@ -11,6 +11,12 @@
 #include <QDesktopServices>
 #include <QAuthenticator>
 
+///-------Json-------------
+#include <QJsonParseError>
+#include <QJsonObject>
+#include <QJsonArray>
+
+
 ProgressDialog::ProgressDialog(const QUrl &url, QWidget *parent):QProgressDialog(parent)
 {
     setWindowTitle(tr("Download Progress"));
@@ -72,9 +78,194 @@ void HttpClient::startRequest(const QUrl &requestedUrl)
     //    connect(m_networkreply, &QNetworkReply::downloadProgress, progressDialog, &ProgressDialog::networkReplyProgress);
     //    connect(m_networkreply, &QNetworkReply::finished, progressDialog, &ProgressDialog::hide);
     //    progressDialog->show();
+}
 
+void HttpClient::ParseDwonData()
+{
+    m_patientstudydb.count = 0;
+    m_patientstudydb.rowinfo.clear();
+    if (m_currentfiletype == DownFileType::dbinfo && m_currentDownData.size() > 1)
+    {
+        QJsonParseError jsonError;
+        QJsonDocument paserDoc = QJsonDocument::fromJson(m_currentDownData, &jsonError);
+        if (jsonError.error == QJsonParseError::NoError)
+        {
+            QJsonObject paserObj = paserDoc.object();
+            if (paserObj.contains("code"))
+            {
+                QJsonValue codeValue = paserObj.take("code");
+                if (codeValue.isDouble())
+                {
+                    m_patientstudydb.code = codeValue.toInt();
+                }
+            }
+            if (paserObj.contains("msg"))
+            {
+                QJsonValue msgValue = paserObj["msg"];
+                if (msgValue.isString())
+                    m_patientstudydb.msg = msgValue.toString();
+            }
+            if (paserObj.contains("count"))
+            {
+                QJsonValue countValue = paserObj["count"];
+                if (countValue.isDouble())
+                    m_patientstudydb.count = countValue.toInt();
+            }
+            if (paserObj.contains("data"))
+            {
+                QJsonValue dataValue = paserObj.take("data");
+                if (dataValue.isArray())
+                {
+                    QJsonArray array = dataValue.toArray();
+                    for(int i = 0; i < array.size(); ++i)
+                    {
+                        StudyRowInfo rowinfo;
+                        QJsonValue tmp = array.at(i);
+                        setPatientDBinfo(tmp,rowinfo);
+                        m_patientstudydb.rowinfo.push_back(rowinfo);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void HttpClient::setPatientDBinfo(QJsonValue &JsonValue, StudyRowInfo &Rowinfo)
+{
+    QJsonObject paserObj = JsonValue.toObject();
+//    QString Name;
+//    QJsonObject::Iterator it;
+//    QString keyString="";
+//    QString valueString="";
+//    int i=0;
+//    for(it=paserObj.begin();it!=paserObj.end();it++)
+//    {
+//        QString value=it.value().toString();
+//        keyString=it.key()+","+keyString;
+//        valueString="'"+value+"',"+valueString;
+//        i++;
+//    }
+    if (paserObj.contains("patientIdentity"))
+    {
+        Rowinfo.patientIdentity = paserObj["patientIdentity"].toString();
+    }
+    if (paserObj.contains("patientName"))
+    {
+        Rowinfo.patientName = paserObj["patientName"].toString();
+    }
+    if (paserObj.contains("patientId"))
+    {
+        Rowinfo.patientId = paserObj["patientId"].toString();
+    }
+    if (paserObj.contains("patientSex"))
+    {
+        Rowinfo.patientSex = paserObj["patientSex"].toString();
+    }
+    if (paserObj.contains("patientBirthday"))
+    {
+        Rowinfo.patientBirthday = paserObj["patientBirthday"].toString();
+    }
+    if (paserObj.contains("patientTelNumber"))
+    {
+        Rowinfo.patientTelNumber = paserObj["patientTelNumber"].toString();
+    }
+    if (paserObj.contains("patientAddr"))
+    {
+        Rowinfo.patientAddr = paserObj["patientAddr"].toString();
+    }
+    if (paserObj.contains("patientCarID"))
+    {
+        Rowinfo.patientCarID = paserObj["patientCarID"].toString();
+    }
+    if (paserObj.contains("patientType"))
+    {
+        Rowinfo.patientType = paserObj["patientType"].toString();
+    }
+    if (paserObj.contains("patientEmail"))
+    {
+        Rowinfo.patientEmail = paserObj["patientEmail"].toString();
+    }
+    if (paserObj.contains("studyOrderIdentity"))
+    {
+        Rowinfo.studyOrderIdentity = paserObj["studyOrderIdentity"].toString();
+    }
+
+    if (paserObj.contains("studyId"))
+    {
+        Rowinfo.studyId = paserObj["studyId"].toString();
+    }
+    if (paserObj.contains("studyuid"))
+    {
+        Rowinfo.studyuid = paserObj["studyuid"].toString();
+    }
+    if (paserObj.contains("scheduledDateTime"))
+    {
+        Rowinfo.scheduledDateTime = paserObj["scheduledDateTime"].toString();
+    }
+
+    if (paserObj.contains("patientEmail"))
+    {
+        Rowinfo.patientEmail = paserObj["patientEmail"].toString();
+    }
+    if (paserObj.contains("ScheduledDate"))
+    {
+        Rowinfo.ScheduledDate = paserObj["ScheduledDate"].toString();
+    }
+    if (paserObj.contains("orderDateTime"))
+    {
+        Rowinfo.orderDateTime = paserObj["orderDateTime"].toString();
+    }
+
+    if (paserObj.contains("studyDescription"))
+    {
+        Rowinfo.studyDescription = paserObj["studyDescription"].toString();
+    }
+    if (paserObj.contains("studyModality"))
+    {
+        Rowinfo.studyModality = paserObj["studyModality"].toString();
+    }
+    if (paserObj.contains("aETitle"))
+    {
+        Rowinfo.aETitle = paserObj["aETitle"].toString();
+    }
+
+    if (paserObj.contains("studyType"))
+    {
+        Rowinfo.studyType = paserObj["studyType"].toString();
+    }
+    if (paserObj.contains("studyCode"))
+    {
+        Rowinfo.studyCode = paserObj["studyCode"].toString();
+    }
+    if (paserObj.contains("studyState"))
+    {
+        Rowinfo.studyState = paserObj["studyState"].toString();
+    }
+
+    if (paserObj.contains("studyCost"))
+    {
+        Rowinfo.studyCost = paserObj["studyCost"].toString();
+    }
+    if (paserObj.contains("studyDate"))
+    {
+        Rowinfo.studyDate = paserObj["studyDate"].toString();
+    }
+    if (paserObj.contains("studyDepart"))
+    {
+        Rowinfo.studyDepart = paserObj["studyDepart"].toString();
+    }
+
+    if (paserObj.contains("sStudyModality"))
+    {
+        Rowinfo.sStudyModality = paserObj["sStudyModality"].toString();
+    }
+    if (paserObj.contains("costType"))
+    {
+        Rowinfo.costType = paserObj["costType"].toString();
+    }
 
 }
+
 
 void HttpClient::getStudyDBinfo(QUrl url,QString start,QString end,QString page,QString limit)
 {
@@ -85,9 +276,9 @@ void HttpClient::getStudyDBinfo(QUrl url,QString start,QString end,QString page,
     //QUrl url = ui->m_URL->text();
     QString newUrlStr = url.toString() + "/healthsystem/ris/stduyimage/?start=";
     newUrlStr += start+"&end="+end+"&page="+page+"&limit="+limit;
-    m_currentfiletype = DownFileType::json;
+    m_currentfiletype = DownFileType::dbinfo;
 
-    const QUrl newUrl = newUrlStr;
+    QUrl newUrl = newUrlStr;
     if (!newUrl.isValid())
     {
         QMessageBox::information(NULL, tr("Error"),tr("Invalid URL: %1: %2").arg(newUrlStr, newUrl.errorString()));
@@ -117,9 +308,12 @@ void HttpClient::downFileFromWeb(QUrl httpUrl, QString savefilename, QString dow
         }
         QFile::remove(fileName);
     }
-    if (!openFileForWrite(fileName))
+    if (m_currentfiletype != DownFileType::dbinfo)
     {
-        return;
+        if (!openFileForWrite(fileName))
+        {
+            return;
+        }
     }
 
     // schedule the request
@@ -150,7 +344,7 @@ void HttpClient::getStudyImageFile(QUrl url,QString studyuid,QString seruid, QSt
         {
             QString strURL = url.toString()+"/WADO?studyuid="+studyuid+"&type=json";
             url = QUrl(strURL);
-            m_currentfiletype = DownFileType::json;
+            m_currentfiletype = DownFileType::studyini;
         }
     }
     m_url = url;
@@ -173,7 +367,7 @@ void HttpClient::getStudyImageFile(QUrl url,QString studyuid,QString seruid, QSt
     case DownFileType::dcm :
         fileName = imguid+".dcm";
         break;
-    case DownFileType::json:
+    case DownFileType::studyini:
         fileName = studyuid+".json";
         break;
     default:
@@ -243,6 +437,7 @@ void HttpClient::httpFinished()
         startRequest(redirectedUrl);
         return;
     }
+    ParseDwonData();
 
     /// test ///QDesktopServices::openUrl(QUrl::fromLocalFile(fileinfo.absoluteFilePath()));
 
@@ -274,18 +469,18 @@ void HttpClient::httpReadyRead()
     // We read all of its new data and write it into the file.
     // That way we use less RAM than when reading it at the finished()
     // signal of the QNetworkReply
-    if (m_file)
+    if  (m_currentfiletype == DownFileType::dbinfo)
     {
-        if (m_currentfiletype == DownFileType::json)
-        {
-            m_currentDownData = m_networkreply->readAll();
-            m_file->write(m_currentDownData);
-        }
-        else
+        m_currentDownData = m_networkreply->readAll();
+    }
+    else
+    {
+        if (m_file)
         {
             m_file->write(m_networkreply->readAll());
         }
     }
+
 }
 
 void HttpClient::slotAuthenticationRequired(QNetworkReply *, QAuthenticator *authenticator)
