c13ac5a4dafb98a490a684b2c876d8d052be4653
zyq1569 <zyq1569@126.com>
2020-10-22 12:14:34 +0800

    HttpClient

--- Tools/Test/TestHttpClient/hthreadobject.cpp
+++ Tools/Test/TestHttpClient/hthreadobject.cpp
@@ -3,14 +3,48 @@
 #include <QNetworkReply>
 #include <QFile>
 #include <QFileInfo>
-
-#define  WAIT_HTTP 0
-#define  DOWN_HTTP 1
+#include <QNetworkAccessManager>
 
 
+ProgressDialog::ProgressDialog(const QUrl &url, QWidget *parent):QProgressDialog(parent)
+{
+    setWindowTitle(tr("Download Progress"));
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setLabelText(tr("Downloading %1.").arg(url.toDisplayString()));
+    setMinimum(0);
+    setValue(0);
+    setMinimumDuration(0);
+    setMinimumSize(QSize(400, 75));
+}
+
+void ProgressDialog::inIt(QUrl url)
+{
+    setWindowTitle(tr("Download Progress"));
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setLabelText(tr("Downloading %1.").arg(url.toDisplayString()));
+    setMinimum(0);
+    setValue(0);
+    setMinimumDuration(0);
+    setMinimumSize(QSize(400, 75));
+}
+
+ProgressDialog::~ProgressDialog()
+{
+
+}
+
+void ProgressDialog::networkReplyProgress(qint64 bytesRead, qint64 totalBytes)
+{
+    setMaximum(totalBytes);
+    setValue(bytesRead);
+}
+
+
+///--------------------------------------------------------------
 HThreadObject::HThreadObject(QObject *parent) : QObject(parent)
 {
     m_file = nullptr;
+    m_networkmanager = nullptr;
 }
 
 
@@ -26,6 +60,24 @@ void HThreadObject::ReadyRead()
         m_file->write(m_networkreply->readAll());
     }
 }
+
+#ifndef QT_NO_SSL
+void HThreadObject::sslErrors(QNetworkReply *, const QList<QSslError> &errors)
+{
+    QString errorString;
+    for (const QSslError &error : errors)
+    {
+        if (!errorString.isEmpty())
+        {
+            errorString += '\n';
+        }
+        errorString += error.errorString();
+    }
+    qDebug() <<errorString;
+    m_networkreply->ignoreSslErrors();
+}
+#endif
+
 void HThreadObject::Finished()
 {
     if (m_file)
@@ -43,11 +95,6 @@ void HThreadObject::Finished()
     m_networkreply->deleteLater();
     m_networkreply = nullptr;
     startNework();
-}
-
-void HThreadObject::setState(int state)
-{
-
 }
 
 void HThreadObject::setInput(QList<HttpInfo> httpInfo)
@@ -75,10 +122,12 @@ void HThreadObject::startNework()
         notifyResult(OpenFile_Fail);
         return ;
     }
-    QUrl url = m_httpInfo[m_index].url;
-    m_networkreply = m_networkmanager->get(QNetworkRequest(url));
+    m_networkreply = m_networkmanager->get(QNetworkRequest(m_httpInfo[m_index].url));
     connect(m_networkreply, &QIODevice::readyRead, this, &HThreadObject::ReadyRead);
     connect(m_networkreply, &QNetworkReply::finished, this, &HThreadObject::Finished);
+#ifndef QT_NO_SSL
+    connect(m_networkmanager, &QNetworkAccessManager::sslErrors, this, &HThreadObject::sslErrors);
+#endif
 
     m_index++;
 }
@@ -86,9 +135,104 @@ void HThreadObject::startNework()
 void HThreadObject::work()
 {
     m_index = 0;
-    m_networkmanager = new QNetworkAccessManager(this);
+    if (!m_networkmanager)
+    {
+        m_networkmanager = new QNetworkAccessManager(this);
+    }
     if (m_networkmanager)
     {
         startNework();
     }
+}
+
+///------------------------------------------------
+HManageThread::HManageThread()
+{
+    for (int i=0; i<Max_thread_size; i++)
+    {
+        worker[i] = new HThreadObject;
+        worker[i]->moveToThread(&workerThread[i]);
+        connect(&workerThread[i], &QThread::finished, worker[i], &QObject::deleteLater);
+        connect(this, &HManageThread::operate, worker[i], &HThreadObject::work);
+        connect(worker[i], &HThreadObject::notifyResult, this, &HManageThread::handleResults);
+        workerThread[i].start();
+    }
+}
+
+HManageThread::~HManageThread()
+{
+    for (int i=0; i<Max_thread_size; i++)
+    {
+        workerThread[i].quit();
+        workerThread[i].wait();
+    }
+}
+
+void HManageThread::start(QList<HttpInfo> httpInfo)
+{
+    int size = httpInfo.size();
+    m_total = size;
+    m_remainder = size;
+    m_fileinfo = "Total "+ QString("%1").arg(m_total) + " files save ok!";
+    int averg = size/Max_thread_size;
+    int mod = size%Max_thread_size;
+    int index = 0;
+    for (int i=0; i<Max_thread_size; i++)
+    {
+        number[i] = averg;
+        if(mod>0)
+        {
+            number[i]++;
+            mod --;
+        }
+        QList<HttpInfo> temp;
+        int max = averg*(i+1);
+        if (max > size)
+        {
+            max = size;
+        }
+        for (; number[i]>0&&index<size; number[i]--)
+        {
+            temp.push_back(httpInfo[index++]);
+        }
+        worker[i]->setInput(temp);
+    }
+    emit operate();
+
+    QUrl url = httpInfo[0].url;
+    QString host = url.host();
+    QString  port = ":"+QString("%1").arg(url.port());
+
+    static ProgressDialog progressDialog(QUrl(host+port), NULL);
+    progressDialog.inIt(QUrl(host+port));
+    progressDialog.setMaximum(m_total);
+    progressDialog.setAttribute(Qt::WA_DeleteOnClose);
+    connect(&progressDialog, &QProgressDialog::canceled, this, &HManageThread::cancelDownload);
+    connect(this, &HManageThread::downloadProgress, &progressDialog, &ProgressDialog::networkReplyProgress);
+    connect(this, &HManageThread::finished, &progressDialog, &ProgressDialog::hide);
+    progressDialog.show();
+//    ProgressDialog *progressDialog = new ProgressDialog(QUrl(host+port), NULL);
+//    progressDialog->inIt(QUrl(host+port));
+//    progressDialog->setAttribute(Qt::WA_DeleteOnClose);
+//    connect(progressDialog, &QProgressDialog::canceled, this, &HManageThread::cancelDownload);
+//    connect(this, &HManageThread::downloadProgress, progressDialog, &ProgressDialog::networkReplyProgress);
+//    connect(this, &HManageThread::finished, progressDialog, &ProgressDialog::hide);
+//    progressDialog->show();
+}
+
+
+void HManageThread::cancelDownload()
+{
+
+}
+
+void HManageThread::handleResults(const int &msg)
+{
+    m_remainder--;
+    if (m_remainder <1)
+    {
+        emit finished();
+        //QMessageBox::question(NULL, tr("Down All file"),m_fileinfo,QMessageBox::Ok );
+    }
+    emit downloadProgress(m_total-m_remainder,m_total);
 }
