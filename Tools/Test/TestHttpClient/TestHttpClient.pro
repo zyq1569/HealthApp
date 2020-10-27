44ece067d3cd016d2b8e138f9290ad0a7a7210db
zyq1569 <zyq1569@126.com>
2020-10-27 16:56:29 +0800

    add hmanagethread.h & hmanagethread.cpp

--- Tools/Test/TestHttpClient/TestHttpClient.pro
+++ Tools/Test/TestHttpClient/TestHttpClient.pro
@@ -16,12 +16,14 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    hmanagethread.cpp \
     hthreadobject.cpp \
     httpclient.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
+    hmanagethread.h \
     hthreadobject.h \
     httpclient.h \
     mainwindow.h
--- Tools/Test/TestHttpClient/hthreadobject.h
+++ Tools/Test/TestHttpClient/hthreadobject.h
@@ -1,7 +1,6 @@
 #ifndef HTHREADOBJECT_H
 #define HTHREADOBJECT_H
 
-#include <QProgressDialog>
 #include <QObject>
 #include <QMutex>
 #include <QThread>
@@ -18,6 +17,9 @@ class QNetworkAccessManager;
 #define  SaveFile_Ok 1
 #define  Finished_All 2
 
+
+
+///---------------down files from "HttpInfo"---thread---
 struct HttpInfo
 {
     QUrl url;
@@ -56,32 +58,6 @@ private:
     QList<HttpInfo> m_httpInfo;
 };
 
-///--------------------------------------------------------------------------------------------
 
-#define  Max_thread_size 3
-class HManageThread : public QObject
-{
-    Q_OBJECT
-    QThread workerThread[Max_thread_size];
-    HThreadObject *worker[Max_thread_size];
-    int number[Max_thread_size];
-public:
-    HManageThread();
-    ~HManageThread();
-    void start(QList<HttpInfo> httpInfo);
-public slots:
-    void handleResults(const int &msg);
-    void cancelDownload();
-signals:
-    void operate();
-    void downloadProgress(qint64 bytesRead, qint64 totalBytes);
-    void ProgressInfo(QString text);
-    void readfiles(qint64 bytesRead);
-    void finished();
-private:
-    int m_total,m_remainder;
-    QString m_fileinfo;
-    QObject *m_parent;
-};
 
 #endif // HTHREADOBJECT_H
