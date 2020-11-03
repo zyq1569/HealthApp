8cc5537ad76a4671210b4c63db0c2f30142ad11b
zyq1569 <zyq1569@126.com>
2020-11-03 21:53:41 +0800

    QLocalSocket IPC

--- Tools/MHealthReport/mainapp.cpp
+++ Tools/MHealthReport/mainapp.cpp
@@ -40,17 +40,37 @@ void MainApp::on_m_SendData_clicked()
 void  MainApp::connectImageApp()
 {
     m_localSocket->connectToServer(ImageAppName);
-    if(m_localSocket->waitForConnected(1000)){
+    if(m_localSocket->waitForConnected(1000))
+    {
         //setOutputText("Connected");
         //qDebug() << "Connect to " + serverName + " succeed.";
-    }else{
+    }else
+    {
         //setOutputText("Connect failed");
         //qDebug() << "Connect to " + serverName + " failed";
     }
 }
 void  MainApp::sendToImageAppMsg()
 {
+    if(m_localSocket->isValid())
+    {
 
+        QString msg = "getMsgText()" ;// + QString::number(val);
+        if(m_localSocket->write(msg.toUtf8()) == -1)
+        {
+            qDebug() << "An error occurred.Send message failed.";
+        }
+        else
+        {
+            qDebug() << "Send message succeed " << msg;
+        }
+        m_localSocket->flush();
+
+    }
+    else
+    {
+        qDebug() << "Does not connect to any Servers";
+    }
 }
 void  MainApp::disconnectImageApp()
 {
