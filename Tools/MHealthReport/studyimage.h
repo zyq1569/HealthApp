63df456f08fe3488f72ef5c236164de4fd12f578
zyq1569 <zyq1569@126.com>
2020-11-10 15:42:53 +0800

    add table: studyimage ui

--- Tools/MHealthReport/studyimage.h
+++ Tools/MHealthReport/studyimage.h
@@ -3,6 +3,13 @@
 
 #include <QMainWindow>
 
+#define ImageAppName  "StarViewer"
+
+class QLocalServer;
+class QLocalSocket;
+class HttpClient;
+
+
 namespace Ui {
 class StudyImage;
 }
@@ -14,6 +21,31 @@ class StudyImage : public QMainWindow
 public:
     explicit StudyImage(QWidget *parent = nullptr);
     ~StudyImage();
+
+public slots:
+    /// socket IPC
+    void connectImageApp();
+    void sendToImageAppMsg(QString data);
+    void disconnectImageApp();
+    void ReadImageApp();
+    void connectImageAppCrash();
+
+
+private slots:
+    /// StudyDB info
+    void on_m_getStudyDbImages_clicked();
+
+    void updateStudyImageTable();
+
+    void on_m_tableWidget_cellDoubleClicked(int row, int column);
+
+signals:
+    void sendClientMsg(QString data);
+
+private:
+    QLocalSocket *m_localSocket;
+    HttpClient *m_httpclient;
+    QString m_url;
 
 private:
     Ui::StudyImage *ui;
