00ea8585e639e7db985ab463400e2478a2323802
zyq1569 <zyq1569@126.com>
2020-11-15 21:21:34 +0800

    register ui

--- Tools/MHealthReport/patientstudyregister.h
+++ Tools/MHealthReport/patientstudyregister.h
@@ -7,6 +7,38 @@ namespace Ui {
 class PatientStudyRegister;
 }
 
+typedef struct StudyData  {
+    QString PatientIdentity
+    PatientName
+    PatientID      ,
+    PatientSex      ,
+    PatientBirthday  ,
+    PatientTelNumber  ,
+    PatientAddr      ,
+    PatientCarID    ,
+    PatientType     ,
+    PatientEmail     ,
+    StudyOrderIdentity,
+    StudyID           ,
+    StudyUID           ,
+    ScheduledDateTime  ,
+    ScheduledDate      ,
+    OrderDateTime     ,
+    StudyDescription  ,
+    StudyModality      ,
+    AETitle           ,
+    StudyType         ,
+    StudyCode         ,
+    StudyState        ,
+    StudyCost          ,
+    StudyDateTime     ,
+    // SstudyID           ,
+    StudyDepart    ,
+    SstudyModality ,
+    // SstudyUID
+    CostType `
+}StudyData;
+
 typedef struct DataPaint
 {
     QString regID;
@@ -59,7 +91,7 @@ public:
     void clearInfo();
 
 public slots:
-//    void flageChange();                           //内容改变
+    //    void flageChange();                           //内容改变
     void on_actionSavePatientInfo_triggered();    //保存患者信息
     void on_actionClearPatientInfo_triggered();   //清除患者信息
 
