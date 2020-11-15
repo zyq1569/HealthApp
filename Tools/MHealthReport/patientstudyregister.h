a799fe1bf619cf879b5d1ba099d571e413fd5e6d
zyq1569 <zyq1569@126.com>
2020-11-15 16:48:36 +0800

    register ui

--- Tools/MHealthReport/patientstudyregister.h
+++ Tools/MHealthReport/patientstudyregister.h
@@ -59,7 +59,7 @@ public:
     void clearInfo();
 
 public slots:
-    void flageChange();                           //内容改变
+//    void flageChange();                           //内容改变
     void on_actionSavePatientInfo_triggered();    //保存患者信息
     void on_actionClearPatientInfo_triggered();   //清除患者信息
 
