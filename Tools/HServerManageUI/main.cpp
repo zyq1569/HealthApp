16944cd223f8b99e04bcff6c87085887c739cf0c
zyq1569 <zyq1569@126.com>
2020-11-16 18:44:23 +0800

    add systray (only for windows!!!!!)

--- Tools/HServerManageUI/main.cpp
+++ Tools/HServerManageUI/main.cpp
@@ -1,6 +1,32 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#ifndef QT_NO_SYSTEMTRAYICON
+#include <QMessageBox>
+int main(int argc, char *argv[])
+{
+    Q_INIT_RESOURCE(systray);
+
+    QApplication a(argc, argv);
+    HMainWindow w;
+    w.show();
+
+    if (!QSystemTrayIcon::isSystemTrayAvailable())
+    {
+        QMessageBox::critical(nullptr, QObject::tr("Systray"),
+                              QObject::tr("I couldn't detect any system tray "
+                                          "on this system."));
+        return 1;
+    }
+    QApplication::setQuitOnLastWindowClosed(false);
+    return a.exec();
+}
+
+#else
+
+#include <QLabel>
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -9,3 +35,4 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
+#endif
