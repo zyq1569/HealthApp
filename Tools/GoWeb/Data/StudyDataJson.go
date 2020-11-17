e9cdd641874c5418ff29fdeb8b10d74620cf78f0
zyq1569 <zyq1569@126.com>
2020-11-17 16:54:31 +0800

    add new get studyorder from DB

--- Tools/GoWeb/Data/StudyDataJson.go
+++ Tools/GoWeb/Data/StudyDataJson.go
@@ -40,38 +40,6 @@ type StudyData struct {
 	CostType string `json:"costType"`
 }
 
-type OrderStudyData struct {
-	PatientIdentity    string `json:"patientIdentity"`
-	PatientName        string `json:"patientName"`
-	PatientNameEnglish string `json:"patientNameEnglish"`
-	PatientID          string `json:"patientId"`
-	PatientSex         string `json:"patientSex"`
-	PatientBirthday    string `json:"patientBirthday"`
-	PatientTelNumber   string `json:"patientTelNumber"`
-	PatientAddr        string `json:"patientAddr"`
-	PatientCarID       string `json:"patientCarID"`
-	PatientType        string `json:"patientType"`
-	PatientEmail       string `json:"patientEmail"`
-	StudyOrderIdentity string `json:"studyOrderIdentity"`
-	StudyID            string `json:"studyId"`
-	StudyUID           string `json:"studyuid"`
-	ScheduledDateTime  string `json:"scheduledDateTime"`
-	ScheduledDate      string `json:"ScheduledDate"`
-	OrderDateTime      string `json:"orderDateTime"`
-	StudyDescription   string `json:"studyDescription"`
-	StudyModality      string `json:"studyModality"`
-	AETitle            string `json:"aETitle"`
-	StudyType          string `json:"studyType"`
-	StudyCode          string `json:"studyCode"`
-	StudyState         string `json:"studyState"`
-	StudyCost          string `json:"studyCost"`
-	StudyDateTime      string `json:"studyDate"`
-	StudyAge           string `json:"StudyAge"`
-	StudyDepart        string `json:"studyDepart"`
-	SstudyModality     string `json:"sStudyModality"`
-	CostType           string `json:"costType"`
-}
-
 type ReportData struct {
 	ReportIdentity     string `json:"ReportIdentity"`
 	StudyOrderIdentity string `json:"StudyOrderIdentity"`
@@ -85,6 +53,46 @@ type ReportData struct {
 	ReportSave         string `json:"ReportSave"`
 	ReportChange       string `json:"ReportChange"`
 	ReportOther        string `json:"ReportOther"`
+}
+
+/// 2020-11-17 add :
+type StudyOrderData struct {
+	/// patient table
+	PatientIdentity    string
+	PatientID          string
+	PatientName        string
+	PatientNameEnglish string
+	PatientSex         string
+	PatientBirthday    string
+	PatientAddr        string
+	PatientEmail       string
+	PatientCarID       string
+	PatientTelNumber   string
+	PatientType        string
+	PatientState       string
+	/// order table
+	StudyOrderIdentity     string
+	PatientIdentity        string
+	StudyID                string
+	StudyUID               string
+	StudyModality          string
+	StudyAge               string
+	ScheduledDateTime      string
+	AETitle                string
+	OrderDateTime          string
+	StudyDescription       string
+	StudyDepart            string
+	StudyCode              string
+	StudyCost              string
+	CostType               string
+	StudyType              string
+	StudyState             string
+	StudyDateTime          string
+	InstitutionName        string
+	ProcedureStepStartDate string
+	StudyModalityIdentity  string
+	StudyManufacturer      string
+	RegisterID             string
 }
 
 type JsonCode struct {
