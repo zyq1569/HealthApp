96168ba9b963bdb3de2b62e7b36dbb118e3cc3e5
zyq1569 <zyq1569@126.com>
2020-11-20 10:37:43 +0800

    1. set Window TOPMOST 2. set Window NOTOPMOST

--- Tools/HServerManageUI/main.cpp
+++ Tools/HServerManageUI/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     HMainWindow w;
+    w.setWindowFlags(w.windowFlags()& ~Qt::WindowMaximizeButtonHint);
     w.show();
 
     if (!QSystemTrayIcon::isSystemTrayAvailable())
