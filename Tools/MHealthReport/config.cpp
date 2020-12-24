b3530e5d3829d2e5c8def443b3629e0e10fcc2c5
zyq1569 <zyq1569@126.com>
2020-12-24 21:06:21 +0800

    add config.ui

--- Tools/MHealthReport/config.cpp
+++ Tools/MHealthReport/config.cpp
@@ -8,6 +8,18 @@ Config::Config(QWidget *parent) :
     ui->setupUi(this);
 }
 
+void Config::setConfig(QString serverip, QString serverport)
+{
+    ui->m_serverIP->setText(serverip);
+    ui->m_serverPort->setText(serverport);
+}
+
+void Config::getConfig(QString &serverip, QString &serverport)
+{
+    serverip = ui->m_serverIP->text();
+    serverport = ui->m_serverPort->text();
+}
+
 Config::~Config()
 {
     delete ui;
