251d7eb15e02403eeae1f97c0415e96270ab15b1
zyq1569 <zyq1569@126.com>
2021-04-23 23:32:48 +0800

    Modified   Tools/GoWeb

--- Tools/GoWeb/Units/Units.go
+++ Tools/GoWeb/Units/Units.go
@@ -2,6 +2,7 @@
 package Units
 
 import (
+	"fmt"
 	"math/rand"
 	"strconv"
 	"strings"
@@ -13,13 +14,13 @@ const (
 )
 
 type OFHashValue struct {
-	first  int32
-	second int32
+	first  string
+	second string
 }
 
 func GetStudyHashDir(studyuid string) string {
 	vl := CreateHashValue([]byte(studyuid), strings.Count(studyuid, "")-1)
-	dir := "/" + strconv.Itoa((int)(vl.first)) + "/" + strconv.Itoa((int)(vl.second))
+	dir := "/" + vl.first + "/" + vl.second
 	return dir
 }
 
@@ -56,37 +57,29 @@ func CreateHashValue(buffer []byte, length int) OFHashValue {
 	//101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179
 	//181 191 193 197 199
 	// var a string = "Runoob"
-	const M1 int32 = 71
-	const M2 int32 = 79
+	var len int = 0
+	len = length
 
-	const D1 int32 = 197
-	const D2 int32 = 199
-	var value int32 = 0
+	const M1 int = 71
+	const M2 int = 79
+
+	const D1 int = 197
+	const D2 int = 199
+	var value int = M1 + D1
 	var hashvalue OFHashValue
 	var i int = 0
-	for i = 0; i < length; i++ {
-		// println("--buffer[i]--")
-		// println(buffer[i])
-		// println(buffer[i] & 0xFF)
-		// println(value * (int32)(M1))
-		value = value*M1 + (int32)(buffer[i])
-		// println(value)
+	for i = 0; i < len; i++ {
+		value = (value+(int)(buffer[i]))*M1 + i*D1
 	}
-	value = value % D1
-	if value < 0 {
-		value = value + D1
-	}
-	hashvalue.first = value
+	hashvalue.first = fmt.Sprintf(hashvalue.first, "%x", value)
 
-	value = 0
-	for i = 0; i < length; i++ {
-		value = (int32)(value*M2) + (int32)(buffer[i])
+	value = M2 + D2
+	for i = 0; i < len; i++ {
+		value = (value+(int)(buffer[i]))*M2 + i*D2
 	}
-	value = value % D2
-	if value < 0 {
-		value = value + D2
-	}
-	hashvalue.second = value
+	hashvalue.second = fmt.Sprintf(hashvalue.first, "%x", value)
+
+	// println(hashvalue)
 	return hashvalue
 }
 
@@ -105,39 +98,4 @@ func CreateHashValue(buffer []byte, length int) OFHashValue {
 // 	//2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
 // 	//101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179
 // 	//181 191 193 197 199
-// 	// var a string = "Runoob"
-// 	println("--length--")
-// 	println(length)
-// 	const M1 uint = 71
-// 	const M2 uint = 79
-
-// 	const D1 uint = 197
-// 	const D2 uint = 199
-// 	var value uint = 0
-// 	var hashvalue OFHashValue
-// 	var i uint = 0
-// 	for i = 0; i < length; i++ {
-// 		println("--buffer[i]--")
-// 		println(buffer[i])
-// 		println((uint)(buffer[i] & 0xFF))
-// 		println(value * M1)
-// 		value = value*M1 + (uint)(buffer[i]&0xFF)
-// 		println(value)
-// 	}
-// 	value = value % D1
-// 	if value < 0 {
-// 		value = value + D1
-// 	}
-// 	hashvalue.first = value
-
-// 	value = 0
-// 	for i = 0; i < length; i++ {
-// 		value = value*M2 + (uint)(buffer[i]&0xFF)
-// 	}
-// 	value = value % D2
-// 	if value < 0 {
-// 		value = value + D2
-// 	}
-// 	hashvalue.second = value
-// 	return hashvalue
 // }
