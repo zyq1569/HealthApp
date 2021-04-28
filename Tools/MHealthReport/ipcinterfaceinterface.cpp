9937d6350dc3093fd620ac38ca8dfab286092197
zyq1569 <zyq1569@126.com>
2021-04-28 21:41:40 +0800

    Modified   Tools/MHealthReport/ipcinterfaceinterface.cpp Modified   Tools/MHealthReport/ipcinterfaceinterface.h

--- Tools/MHealthReport/ipcinterfaceinterface.cpp
+++ Tools/MHealthReport/ipcinterfaceinterface.cpp
@@ -1,7 +1,7 @@
 #include "ipcinterfaceinterface.h"
 
-IPCInterfaceInterface::IPCInterfaceInterface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent = nullptr)
-    :QDBusAbstractInterface(service, path, staticInterfaceName(), connection, parent)
+IPCInterfaceInterface::IPCInterfaceInterface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent)
+    :QDBusAbstractInterface(service, path, IPCInterfaceName.toLatin1().data(), connection, parent)
 {
 
 }
