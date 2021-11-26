985b159935fe1eac62a89c696066c994de22537a
zyq1569 <zyq1569@126.com>
2021-11-26 14:59:34 +0800

    Modified   Tools/HServerManageUI/logging.cpp

--- Tools/HServerManageUI/logging.cpp
+++ Tools/HServerManageUI/logging.cpp
@@ -7,18 +7,25 @@
 
 namespace udg {
 
+const char* getPID(const el::LogMessage*)
+{
+    static QString pid = QString::number(getpid());
+    return  pid.toLatin1();
+}
+
 void setLogDefault()
 {
     el::Configurations defaultConf;
     defaultConf.setToDefault();
+	el::Helpers::installCustomFormatSpecifier(el::CustomFormatSpecifier("%pid", getPID));
     // Values are always std::string
     //defaultConf.set(el::Level::Info, el::ConfigurationType::Format, "%datetime{%Y-%M-%d %H:%m:%s} %level %msg");
-    defaultConf.set(el::Level::Warning, el::ConfigurationType::Format, "%datetime{%Y-%M-%d %H:%m:%s} %level %msg");
+    defaultConf.set(el::Level::Warning, el::ConfigurationType::Format, "%datetime{%Y-%M-%d %H:%m:%s:%g} %pid %thread %levshort %msg");
     // default logger uses default configurations
     el::Loggers::reconfigureLogger("default", defaultConf);
     //LOG(INFO) << "Log using default file";
     // To set GLOBAL configurations you may use
-    defaultConf.setGlobally( el::ConfigurationType::Format, "%datetime{%Y-%M-%d %H:%m:%s} %level %msg");
+    defaultConf.setGlobally( el::ConfigurationType::Format, "%datetime{%Y-%M-%d %H:%m:%s:%g} %pid %thread %levshort %msg");
     defaultConf.setGlobally(el::ConfigurationType::Filename, getLogFilePath().toStdString());
     el::Loggers::reconfigureLogger("default", defaultConf);
     INFO_LOG("setLogDefault()---INFO!");
