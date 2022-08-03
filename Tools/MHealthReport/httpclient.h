e91f83967980b1aad5364071a8658639f348f071
zyq1569 <zyq1569@126.com>
2022-08-03 22:48:38 +0800

    fine-tune

--- Tools/MHealthReport/httpclient.h
+++ Tools/MHealthReport/httpclient.h
@@ -227,10 +227,53 @@ class NetWorkObject: public QObject
     Q_OBJECT
 public:
     NetWorkObject();
+
 public slots:
     void slot();
 
 };
 
+
+//class Dummy:public QObject
+//{
+//    Q_OBJECT
+//public:
+//    Dummy(QObject* parent=0):QObject(parent)     {}
+//public slots:
+//    void emitsig()
+//    {
+//        emit sig();
+//    }
+//signals:
+//    void sig();
+//};
+
+//class Object:public QObject
+//{
+//    Q_OBJECT
+//public:
+//    Object(){}
+//public slots:
+//    void slot()
+//    {
+//        qDebug()<<"from thread slot:" <<QThread::currentThreadId();
+//    }
+//};
+
+//#include "main.moc"
+
+//int main(int argc, char *argv[])
+//{
+//    QCoreApplication a(argc, argv);
+//    qDebug()<<"main thread:"<<QThread::currentThreadId();
+//    QThread thread;
+//    Object obj;
+//    Dummy dummy;
+//    obj.moveToThread(&thread);
+//    QObject::connect(&dummy, SIGNAL(sig()), &obj, SLOT(slot()));
+//    thread.start();
+//    dummy.emitsig();
+//    return a.exec();
+//}
 
 #endif // HTTPCLIENT_H
