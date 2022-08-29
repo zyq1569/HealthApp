d770e7eb3af9ac4dd9113b1bbfd093d43a808444
zyq1569 <zyq1569@126.com>
2022-08-29 22:22:42 +0800

    fine-tune

--- dcm/dcmUnits/Units.cpp
+++ dcm/dcmUnits/Units.cpp
@@ -80,12 +80,12 @@ OFString HashValue(int F, int S, OFString str)
 ///依据常规的hash 算法方式，目前不考虑使用强加密不可逆方式
 //参考 https://github.com/Tessil/hopscotch-map 类似很多，注意协议
 //!根据字符计算两个Hash数值  to do user uint64
-OFHashValue CreateHashValue(OFString str)
+OFHashValue CreateHashValue(DicomFileInfo dcminfo )
 {
     //2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
     //101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179
     //181 191 193 197 199
-
+    OFString str = dcminfo.studyUID;
     OFHashValue hashvalue;
     //71 197
     hashvalue.first  = HashValue(71, 197, str);
