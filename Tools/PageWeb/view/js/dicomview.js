c68418ad1e55815d6e87fc428e8d980328b1d5d2
zyq1569 <zyq1569@126.com>
2022-09-11 22:44:41 +0800

    fine-tune

--- Tools/PageWeb/view/js/dicomview.js
+++ Tools/PageWeb/view/js/dicomview.js
@@ -70,52 +70,51 @@ ImageViewer = function (root, viewport) {
   };
 };
 
-    /**
-     * sets a new image object for a given element
-     * @param element
-     * @param image
-     */
+/**
+ * sets a new image object for a given element
+ * @param element
+ * @param image
+ */
 function displayImage(element, image, viewport) {
-  if(element === undefined) {
-      throw "displayImage: parameter element cannot be undefined";
+  if (element === undefined) {
+    throw "displayImage: parameter element cannot be undefined";
   }
-  if(image === undefined) {
-      throw "displayImage: parameter image cannot be undefined";
+  if (image === undefined) {
+    throw "displayImage: parameter image cannot be undefined";
   }
 
   var enabledElement = cornerstone.getEnabledElement(element);
 
   enabledElement.image = image;
 
-  if(enabledElement.viewport === undefined) {
-      enabledElement.viewport = cornerstone.internal.getDefaultViewport(enabledElement.canvas, image);
+  if (enabledElement.viewport === undefined) {
+    enabledElement.viewport = cornerstone.internal.getDefaultViewport(enabledElement.canvas, image);
   }
 
   // merge viewport
-  if(viewport) {
-      for(var attrname in viewport)
-      {
-          if(viewport[attrname] !== null) {
-              enabledElement.viewport[attrname] = viewport[attrname];
-          }
+  if (viewport) {
+    for (var attrname in viewport) {
+      if (viewport[attrname] !== null) {
+        enabledElement.viewport[attrname] = viewport[attrname];
       }
+    }
   }
 
   var now = new Date();
   var frameRate;
-  if(enabledElement.lastImageTimeStamp !== undefined) {
-      var timeSinceLastImage = now.getTime() - enabledElement.lastImageTimeStamp;
-      frameRate = (1000 / timeSinceLastImage).toFixed();
+  if (enabledElement.lastImageTimeStamp !== undefined) {
+    var timeSinceLastImage = now.getTime() - enabledElement.lastImageTimeStamp;
+    frameRate = (1000 / timeSinceLastImage).toFixed();
   } else {
   }
   enabledElement.lastImageTimeStamp = now.getTime();
 
   var newImageEventData = {
-      viewport : enabledElement.viewport,
-      element : enabledElement.element,
-      image : enabledElement.image,
-      enabledElement : enabledElement,
-      frameRate : frameRate
+    viewport: enabledElement.viewport,
+    element: enabledElement.element,
+    image: enabledElement.image,
+    enabledElement: enabledElement,
+    frameRate: frameRate
   };
 
   $(enabledElement.element).trigger("CornerstoneNewImage", newImageEventData);
