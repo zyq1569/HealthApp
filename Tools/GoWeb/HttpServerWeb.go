2f096924585560d59b7a74d654e258b6580ba4fc
zyq1569 <zyq1569@126.com>
2023-11-27 17:45:07 +0800

    save report

--- Tools/GoWeb/HttpServerWeb.go
+++ Tools/GoWeb/HttpServerWeb.go
@@ -509,7 +509,7 @@ func SaveOdtReport(c echo.Context) error {
 		}
 		///
 		reportIdentity := reportdata.ReportIdentity
-		fileName := CONFIG[IMAGE_Dir] + "/Report//Report/" + reportIdentity + ".odt"
+		fileName := CONFIG[IMAGE_Dir] + "/Report/" + reportIdentity + ".odt"
 		f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE /*|os.O_TRUNC*/, 0666)
 		defer f.Close()
 		if err != nil {
