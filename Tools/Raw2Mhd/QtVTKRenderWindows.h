011013f529e4add189d0718ce0ac32a7105eb6c7
zyq1569 <zyq1569@126.com>
2025-02-24 21:09:18 +0800

    add file

--- Tools/Raw2Mhd/QtVTKRenderWindows.h
+++ Tools/Raw2Mhd/QtVTKRenderWindows.h
@@ -1,66 +1,24 @@
-// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-// SPDX-License-Identifier: BSD-3-Clause
-#ifndef QtVTKRenderWindows_H
-#define QtVTKRenderWindows_H
+#ifndef QTVTKRENDERWINDOWS_H
+#define QTVTKRENDERWINDOWS_H
 
-#include "vtkDistanceWidget.h"
-#include "vtkImagePlaneWidget.h"
-#include "vtkResliceImageViewer.h"
-#include "vtkResliceImageViewerMeasurements.h"
-#include "vtkSmartPointer.h"
 #include <QMainWindow>
 
-class vtkCornerAnnotation;
-class vtkResliceCursorLineRepresentation;
-class vtkMPRResliceImageViewer;
-// Forward Qt class declarations
-class Ui_QtVTKRenderWindows;
+QT_BEGIN_NAMESPACE
+namespace Ui { class QtVTKRenderWindows; }
+QT_END_NAMESPACE
 
 class QtVTKRenderWindows : public QMainWindow
 {
-  Q_OBJECT
-public:
-  // Constructor/Destructor
-  QtVTKRenderWindows(int argc, char* argv[]);
-  ~QtVTKRenderWindows() override {}
+    Q_OBJECT
 
 public:
-	vtkCornerAnnotation *m_cornerAnnotations[3];
-
-public Q_SLOTS:
-
-  virtual void slotExit();
-  virtual void resliceMode(int);
-  virtual void thickMode(int);
-  virtual void SetBlendModeToMaxIP();
-  virtual void SetBlendModeToMinIP();
-  virtual void SetBlendModeToMeanIP();
-  virtual void SetBlendMode(int);
-  virtual void ResetViews();
-  virtual void Render();
-  virtual void AddDistanceMeasurementToView1();
-  virtual void AddDistanceMeasurementToView(int);
-
-protected:
-  //vtkSmartPointer<vtkResliceImageViewer> riw[3];
-  vtkSmartPointer<vtkMPRResliceImageViewer> riw[3];
-	
-  vtkSmartPointer<vtkImagePlaneWidget> planeWidget[3];
-  vtkSmartPointer<vtkDistanceWidget> DistanceWidget[3];
-  vtkSmartPointer<vtkResliceImageViewerMeasurements> ResliceMeasurements;
-
-public:
-	vtkSmartPointer< vtkResliceCursorLineRepresentation > vtkrclp[3];
-
-protected Q_SLOTS:
+    QtVTKRenderWindows(QWidget *parent = nullptr);
+    ~QtVTKRenderWindows();
 
 private slots:
-  void on_pushButton_clicked();
-  void on_Sagittal_clicked();
   void raw2mhd();
-private:
-  // Designer form
-  Ui_QtVTKRenderWindows* ui;
-};
 
-#endif // QtVTKRenderWindows_H
+private:
+    Ui::QtVTKRenderWindows *ui;
+};
+#endif // QTVTKRENDERWINDOWS_H
---
+++ Tools/Raw2Mhd/qtvtkrenderwindows.cpp
@@ -0,0 +1,120 @@
+#include "qtvtkrenderwindows.h"
+#include "ui_qtvtkrenderwindows.h"
+
+#include <vtkImageData.h>
+#include <vtkNew.h>
+#include <vtkSmartPointer.h>
+#include <vtkImageReader2.h>
+#include <fstream>
+#include <iostream>
+
+#include<Qdir>
+
+QtVTKRenderWindows::QtVTKRenderWindows(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::QtVTKRenderWindows)
+{
+    ui->setupUi(this);
+
+	connect(ui->m_pbRaw2Mhd, SIGNAL(clicked()), this, SLOT(raw2mhd()));
+
+	QString dir = QCoreApplication::applicationDirPath();
+	ui->m_dcmDIR->setText(dir);
+
+}
+
+QtVTKRenderWindows::~QtVTKRenderWindows()
+{
+    delete ui;
+}
+
+void QtVTKRenderWindows::raw2mhd()
+{
+    QString dir = ui->m_dcmDIR->toPlainText();
+    QString w = ui->m_width->toPlainText();
+    QString h = ui->m_height->toPlainText();
+    QString number = ui->m_number->toPlainText();
+    QString filenameExtra="";
+    if (ui->m_extraCheck->isChecked())
+    {
+        filenameExtra = ui->m_extra->toPlainText();
+    }
+    int width = w.toInt();
+    int height = h.toInt();
+    int depth = number.toInt();  // 0~200 共 201 张
+    // 逐个读取 0～200 的 RAW 文件
+    QString raw = dir + "VTK_Data.raw";
+    std::string rawFilename = qPrintable(raw);// "D:/TEMP/dingliang/volume_1/volume_VTKData600.raw";  // `.raw` 文件
+    QString mhd = dir + "VTK_Data.mhd";
+    std::string mhdFilename = qPrintable(mhd);// "D:/TEMP/dingliang/volume_1/volume_VTKData600.mhd";  // `.mhd` 文件
+    std::ofstream rawFile(rawFilename, std::ios::binary | std::ios::out | std::ios::trunc);
+    if (!rawFile.is_open())
+    {
+        //std::cerr << "无法打开 .raw 文件进行写入！" << std::endl;
+        return ;
+    }
+    QString fn = ui->m_filename->toPlainText();
+    QString fa = ui->m_fmn->toPlainText();
+    int formateN = fa.toInt();
+    int index_s = ui->m_start->toPlainText().toInt();
+    int index_e = index_s + depth - 1;
+
+    for (int i = index_s; i < index_e; i++)
+    {
+        QString index = dir + "\\" + fn;//
+        if (ui->m_formate->isChecked())
+        {
+            index += QString("%1").arg(i, formateN, 10, QLatin1Char('0'));
+            index += filenameExtra;
+        }
+        else
+        {
+            index += QString("%1").arg(i);
+            index += filenameExtra;
+        }
+
+        std::string filename = qPrintable(index);
+        QFileInfo file(index);
+        if (!file.isFile())
+        {
+            return;
+        }
+        vtkSmartPointer<vtkImageReader2> reader = vtkSmartPointer<vtkImageReader2>::New();
+        reader->SetFileName(filename.c_str());
+        reader->SetFileDimensionality(2);  // 2D 图像
+        reader->SetDataExtent(0, width - 1, 0, height - 1, 0, 0);  // 2D 读取
+        reader->SetDataScalarTypeToUnsignedShort();  // 16-bit 无符号整型
+        reader->SetNumberOfScalarComponents(1);
+        reader->SetFileLowerLeft(true);  // 数据从左下角开始
+        // 设置像素间距
+        reader->SetDataSpacing(0.025, 0.025, 0.025); // X, Y, Z 间距匹配 Scale_x/y/z
+        reader->Update();
+
+        vtkSmartPointer<vtkImageData> imageData = reader->GetOutput();
+        int* dims = imageData->GetDimensions();
+        int dataSize = dims[0] * dims[1] * sizeof(unsigned short);
+
+        rawFile.write(static_cast<char*>(imageData->GetScalarPointer()), dataSize);
+        //std::cout << "已写入切片 " << 10 << " 到 " << std::endl;
+
+    }
+
+    // 生成 MHD 头文件（只执行一次）
+    std::ofstream mhdFile(mhdFilename);
+    if (!mhdFile.is_open())
+    {
+        //std::cerr << "无法创建 .mhd 文件！" << std::endl;
+        return ;
+    }
+    mhdFile << "ObjectType = Image\n";
+    mhdFile << "NDims = 3\n";
+    mhdFile << "DimSize = " << width << " " << height << " " << depth << "\n";
+    mhdFile << "ElementSpacing = " << 0.025 << " " << 0.025 << " " << 0.025 << "\n";
+    mhdFile << "ElementType = MET_USHORT\n";
+    mhdFile << "ElementDataFile = " << rawFilename << "\n";
+    mhdFile.close();
+
+    //std::cout << "MHD 文件已生成：" << mhdFilename << std::endl;
+
+}
+
