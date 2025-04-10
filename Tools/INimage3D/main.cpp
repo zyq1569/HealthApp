6338046e9880063db7db3952d4d26ae9e8acf7cb
zyq1569 <zyq1569@126.com>
2025-04-10 16:21:01 +0800

    fine-tune

--- Tools/INimage3D/main.cpp
+++ Tools/INimage3D/main.cpp
@@ -1,10 +1,40 @@
 #include "mainwindow.h"
 
+#include <Windows.h>
 #include <QApplication>
 #include <QFileInfo>
 
+RECT GetMonitorRectBounds(int i)
+{
+    DISPLAY_DEVICEW d;
+    d.cb = sizeof(d);
+    bool flag = EnumDisplayDevicesW(nullptr, i, &d, 0);
+    DEVMODEW dm;
+    dm.dmSize = sizeof(dm);
+    dm.dmDriverExtra = 0;
+    flag = EnumDisplaySettingsExW(d.DeviceName, ENUM_CURRENT_SETTINGS, &dm, 0);
+    RECT rect = { dm.dmPosition.x,dm.dmPosition.y, dm.dmPosition.x + dm.dmPelsWidth,dm.dmPosition.y + dm.dmPelsHeight };
+    return rect;
+}
+
 int main(int argc, char *argv[])
 {
+    int monitorCount = ::GetSystemMetrics(SM_CMONITORS);
+    QString scaleFactor;
+    for (int i = 0; i < monitorCount; i++)
+    {
+        RECT dims = GetMonitorRectBounds(i);
+        int h = dims.bottom - dims.top;
+        double scale = (h > 1000) ? ((double)h / 1000.0) : 1.0;
+        scaleFactor += (i == 0 ? "" : ";") + QString::number(scale, 'f', 1);
+    }
+
+    QByteArray env = scaleFactor.toUtf8();
+    qputenv("QT_SCREEN_SCALE_FACTORS", env);
+
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
 	QApplication a(argc, argv);
 	MainWindow w;
 	if (argc > 1)
