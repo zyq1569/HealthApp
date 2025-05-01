f8369cbcc6e0ae24a2df42273e5543884c235c0c
zyq1569 <zyq1569@126.com>
2025-05-01 23:45:30 +0800

    fine-tune

---
+++ Tools/INimage3D/volumedataset.h
@@ -0,0 +1,35 @@
+#ifndef VOLUMEDATASET_H
+#define VOLUMEDATASET_H
+
+#include <QWidget>
+
+namespace Ui {
+class VolumeDataSet;
+}
+
+class VolumeDataSet : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit VolumeDataSet(QWidget *parent = nullptr);
+    ~VolumeDataSet();
+
+signals:
+    void SplitImageData(int *dims, int start, int end);
+
+public slots:
+    void SaveSplitParm();
+
+public:
+    void SetSlicesNumber(int *dim);
+
+public:
+    int *m_dims, m_slicesNumber, m_topStart, m_topEnd, m_centerStart, m_centerEnd, m_bottomStart, m_bottomEnd;
+    int m_showIndex;
+
+private:
+    Ui::VolumeDataSet *ui;
+};
+
+#endif // VOLUMEDATASET_H
