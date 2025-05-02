1a4411c2eb143b2f80e75cd2718aa32cb730fe9d
zyq1569 <zyq1569@126.com>
2025-05-02 11:29:40 +0800

    add :VolumeDataSet

--- Tools/INimage3D/mainwindow.cpp
+++ Tools/INimage3D/mainwindow.cpp
@@ -248,9 +248,5 @@ void MainWindow::showImage4Plane()
 
 void MainWindow::closeEvent(QCloseEvent *event)
 {
-    //if (m_vtkImageData && ((QFourpaneviewer*)m_image4Plane)->m_volumeDataSet)
-    //{
-    //    ((QFourpaneviewer*)m_image4Plane)->m_volumeDataSet->hide();
-    //}
     event->accept();
 }
