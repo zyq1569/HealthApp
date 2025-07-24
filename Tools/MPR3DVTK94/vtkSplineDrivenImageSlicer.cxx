2d8119ce34695f975663169f75a8f0f17e465ece
zyq1569 <zyq1569@126.com>
2025-07-25 07:48:15 +0800

    add

--- Tools/MPR3DVTK94/vtkSplineDrivenImageSlicer.cxx
+++ Tools/MPR3DVTK94/vtkSplineDrivenImageSlicer.cxx
@@ -1,3 +1,25 @@
+// Copyright (c) 2010, Jérôme Velut
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT OWNER ``AS IS'' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE COPYRIGHT OWNER BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include "vtkSplineDrivenImageSlicer.h"
 
 #include"vtkPoints.h"
@@ -21,27 +43,27 @@
 #include "vtkStreamingDemandDrivenPipeline.h"
 
 
-//vtkCxxRevisionMacro(vtkSplineDrivenImageSlicer, "$Revision: 1.31 $");
 vtkStandardNewMacro(vtkSplineDrivenImageSlicer);
 
 vtkSplineDrivenImageSlicer::vtkSplineDrivenImageSlicer()
 {
     this->localFrenetFrames = vtkFrenetSerretFrame::New();
-    this->reslicer          = vtkImageReslice::New();
-    this->SliceExtent[0]    = 15;
-    this->SliceExtent[1]    = 15;
-    this->SliceSpacing[0]   = 1;
-    this->SliceSpacing[1]   = 1;
-    this->SliceThickness    = 1;
-    this->OffsetPoint       = 0;
-    this->OffsetLine        = 0;
-    this->ProbeInput        = 0;
+    this->reslicer = vtkImageReslice::New();
+    this->SliceExtent[0] = 15;
+    this->SliceExtent[1] = 15;
+    this->SliceSpacing[0] = 1;
+    this->SliceSpacing[1] = 1;
+    this->SliceThickness = 1;
+    this->OffsetPoint = 0;
+    this->OffsetLine = 0;
+    this->ProbeInput = 0;
 
     this->SetNumberOfInputPorts(2);
     this->SetNumberOfOutputPorts(2);
 
     // by default process active point scalars
-    this->SetInputArrayToProcess(0, 0, 0, vtkDataObject::FIELD_ASSOCIATION_POINTS, vtkDataSetAttributes::SCALARS);
+    this->SetInputArrayToProcess(0, 0, 0, vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS);
 }
 
 vtkSplineDrivenImageSlicer::~vtkSplineDrivenImageSlicer()
@@ -71,7 +93,8 @@ void vtkSplineDrivenImageSlicer::SetPathConnection(int id, vtkAlgorithmOutput* a
     }
     else if (algOutput)
     {
-        vtkWarningMacro("The source id provided is larger than the maximum "  "source id, using " << numConnections << " instead.");
+        vtkWarningMacro("The source id provided is larger than the maximum "
+            "source id, using " << numConnections << " instead.");
         this->AddInputConnection(1, algOutput);
     }
 }
@@ -80,15 +103,19 @@ void vtkSplineDrivenImageSlicer::SetPathConnection(int id, vtkAlgorithmOutput* a
 int vtkSplineDrivenImageSlicer::FillInputPortInformation(int port, vtkInformation *info)
 {
     if (port == 0)
+    {
         info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkImageData");
+    }
     else
+    {
         info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
-
+    }
     return 1;
 }
 
 //----------------------------------------------------------------------------
-int vtkSplineDrivenImageSlicer::FillOutputPortInformation(  int port, vtkInformation* info)
+int vtkSplineDrivenImageSlicer::FillOutputPortInformation(
+    int port, vtkInformation* info)
 {
     if (port == 0)
         info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkImageData");
@@ -98,40 +125,52 @@ int vtkSplineDrivenImageSlicer::FillOutputPortInformation(  int port, vtkInforma
 }
 
 
-int vtkSplineDrivenImageSlicer::RequestInformation( vtkInformation * vtkNotUsed(request),  vtkInformationVector** inputVector,  vtkInformationVector *outputVector)
+int vtkSplineDrivenImageSlicer::RequestInformation(
+    vtkInformation * vtkNotUsed(request),
+    vtkInformationVector**,
+    vtkInformationVector *outputVector)
 {
+
     // get the info objects
     vtkInformation* outInfo = outputVector->GetInformationObject(0);
 
-    int extent[6]     = { 0, this->SliceExtent[0] - 1, 0, this->SliceExtent[1] - 1,0, 1 };
-    //int extent[6] = { 0, this->SliceExtent[0] , 0, this->SliceExtent[1],0, 1 };
+    int extent[6] = { 0, this->SliceExtent[0] - 1,
+                     0, this->SliceExtent[1] - 1,
+                     0, 1 };
     double spacing[3] = { this->SliceSpacing[0],this->SliceSpacing[1],this->SliceThickness };
 
 
     outInfo->Set(vtkDataObject::SPACING(), spacing, 3);
-    outInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(), extent, 6);
+    outInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(),
+        extent, 6);
 
     return 1;
 }
 
 //! RequestData is called by the pipeline process. 
-int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),vtkInformationVector **inputVector,vtkInformationVector *outputVector)
+int vtkSplineDrivenImageSlicer::RequestData(
+    vtkInformation *vtkNotUsed(request),
+    vtkInformationVector **inputVector,
+    vtkInformationVector *outputVector)
 {
     // get the info objects
-    vtkInformation *inInfo       = inputVector[0]->GetInformationObject(0);
-    vtkInformation *pathInfo     = inputVector[1]->GetInformationObject(0);
+    vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+    vtkInformation *pathInfo = inputVector[1]->GetInformationObject(0);
     vtkInformation *outImageInfo = outputVector->GetInformationObject(0);
     vtkInformation *outPlaneInfo = outputVector->GetInformationObject(1);
 
     // get the input and ouptut
-    vtkImageData *input     = vtkImageData::SafeDownCast(inInfo->Get(vtkDataObject::DATA_OBJECT()));
-    int *dim = input->GetDimensions();
-    vtkImageData *inputCopy = vtkImageData::New();
+    vtkImageData *input = vtkImageData::SafeDownCast(
+        inInfo->Get(vtkDataObject::DATA_OBJECT()));
+    vtkSmartPointer<vtkImageData> inputCopy = vtkSmartPointer<vtkImageData>::New();
     inputCopy->ShallowCopy(input);
-    vtkPolyData *inputPath  = vtkPolyData::SafeDownCast(pathInfo->Get(vtkDataObject::DATA_OBJECT()));
+    vtkPolyData *inputPath = vtkPolyData::SafeDownCast(
+        pathInfo->Get(vtkDataObject::DATA_OBJECT()));
 
-    vtkImageData *outputImage = vtkImageData::SafeDownCast( outImageInfo->Get(vtkDataObject::DATA_OBJECT()));
-    vtkPolyData *outputPlane  = vtkPolyData::SafeDownCast( outPlaneInfo->Get(vtkDataObject::DATA_OBJECT()));
+    vtkImageData *outputImage = vtkImageData::SafeDownCast(
+        outImageInfo->Get(vtkDataObject::DATA_OBJECT()));
+    vtkPolyData *outputPlane = vtkPolyData::SafeDownCast(
+        outPlaneInfo->Get(vtkDataObject::DATA_OBJECT()));
 
     vtkSmartPointer<vtkPolyData> pathCopy = vtkSmartPointer<vtkPolyData>::New();
     pathCopy->ShallowCopy(inputPath);
@@ -144,31 +183,32 @@ int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),
     this->localFrenetFrames->Update();
 
     // path will contain PointData array "Tangents" and "Vectors"
-    vtkPolyData* path = static_cast<vtkPolyData*>(this->localFrenetFrames->GetOutputDataObject(0));
+    vtkPolyData* path = static_cast<vtkPolyData*>
+        (this->localFrenetFrames->GetOutputDataObject(0));
     // Count how many points are used in the cells
     // In case of loop, points may be used several times
     // (note: not using NumberOfPoints because we want only LINES points...)
     vtkCellArray* lines = path->GetLines();
     lines->InitTraversal();
-    int numberOfPoints = 0;
+
     vtkIdType nbCellPoints;
+#ifdef VTK_CELL_ARRAY_V2
+    const vtkIdType *points;
+#else // VTK_CELL_ARRAY_V2
     vtkIdType* points;
+#endif // VTK_CELL_ARRAY_V2
 
     vtkIdType cellId = -1;
-    do
-    {
-        lines->GetNextCell(nbCellPoints, (vtkIdType const*&)points);
+    do {
+        lines->GetNextCell(nbCellPoints, points);
         cellId++;
-    } 
-    while (cellId != this->OffsetLine);
+    } while (cellId != this->OffsetLine);
 
     int ptId = this->OffsetPoint;
     if (ptId >= nbCellPoints)
     {
         ptId = nbCellPoints - 1;
     }
-        
-
     // Build a new reslicer with image input as input too.
     this->reslicer->SetInputData(inputCopy);
 
@@ -178,15 +218,18 @@ int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),
     // - normal N
     double center[3];
     path->GetPoints()->GetPoint(ptId, center);
-    vtkDoubleArray* pathTangents = static_cast<vtkDoubleArray*>(path->GetPointData()->GetArray("FSTangents"));
+    vtkDoubleArray* pathTangents = static_cast<vtkDoubleArray*>
+        (path->GetPointData()->GetArray("FSTangents"));
     double tangent[3];
     pathTangents->GetTuple(ptId, tangent);
 
-    vtkDoubleArray* pathNormals  = static_cast<vtkDoubleArray*>(path->GetPointData()->GetArray("FSNormals"));
+    vtkDoubleArray* pathNormals = static_cast<vtkDoubleArray*>
+        (path->GetPointData()->GetArray("FSNormals"));
     double normal[3];
     pathNormals->GetTuple(ptId, normal);
 
-    vtkDoubleArray* pathBinormals = static_cast<vtkDoubleArray*>(path->GetPointData()->GetArray("FSBinormals"));
+    vtkDoubleArray* pathBinormals = static_cast<vtkDoubleArray*>
+        (path->GetPointData()->GetArray("FSBinormals"));
     double binormal[3];
     pathBinormals->GetTuple(ptId, binormal);
 
@@ -199,14 +242,15 @@ int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),
     for (int comp = 0; comp < 3; comp++)
     {
         planeOrigin[comp] = center[comp] - normal[comp] * this->SliceExtent[1] * this->SliceSpacing[1] / 2.0
-                             - binormal[comp] * this->SliceExtent[0] * this->SliceSpacing[0] / 2.0;
+            - binormal[comp] * this->SliceExtent[0] * this->SliceSpacing[0] / 2.0;
         planePoint1[comp] = planeOrigin[comp] + binormal[comp] * this->SliceExtent[0] * this->SliceSpacing[0];
         planePoint2[comp] = planeOrigin[comp] + normal[comp] * this->SliceExtent[1] * this->SliceSpacing[1];
     }
     plane->SetOrigin(planeOrigin);
     plane->SetPoint1(planePoint1);
     plane->SetPoint2(planePoint2);
-    plane->SetResolution(this->SliceExtent[0], this->SliceExtent[1]);
+    plane->SetResolution(this->SliceExtent[0],
+        this->SliceExtent[1]);
     plane->Update();
 
     if (this->ProbeInput == 1)
@@ -221,7 +265,7 @@ int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),
     {
         outputPlane->DeepCopy(plane->GetOutputDataObject(0));
     }
-        
+
     // Build the transformation matrix (inspired from vtkImagePlaneWidget)
     vtkMatrix4x4* resliceAxes = vtkMatrix4x4::New();
     resliceAxes->Identity();
@@ -240,7 +284,7 @@ int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),
         resliceAxes->SetElement(2, comp, tangent[comp]);
 
         origin[comp] = center[comp] - normal[comp] * this->SliceExtent[1] * this->SliceSpacing[1] / 2.0
-                        - binormal[comp] * this->SliceExtent[0] * this->SliceSpacing[0] / 2.0;
+            - binormal[comp] * this->SliceExtent[0] * this->SliceSpacing[0] / 2.0;
     }
 
     //! Transform the origin in the homogeneous coordinate space. 
@@ -266,8 +310,12 @@ int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),
 
     this->reslicer->SetOutputDimensionality(2);
     this->reslicer->SetOutputOrigin(0, 0, 0);
-    this->reslicer->SetOutputExtent(0, this->SliceExtent[0] - 1, 0, this->SliceExtent[1] - 1,  0, 1);
-    this->reslicer->SetOutputSpacing(this->SliceSpacing[0],  this->SliceSpacing[1],  this->SliceThickness);
+    this->reslicer->SetOutputExtent(0, this->SliceExtent[0] - 1,
+        0, this->SliceExtent[1] - 1,
+        0, 1);
+    this->reslicer->SetOutputSpacing(this->SliceSpacing[0],
+        this->SliceSpacing[1],
+        this->SliceThickness);
     this->reslicer->Update();
 
     resliceAxes->Delete();
@@ -279,4 +327,3 @@ int vtkSplineDrivenImageSlicer::RequestData(vtkInformation *vtkNotUsed(request),
     return 1;
 
 }
-
--- Tools/MPR3DVTK94/vtkSplineDrivenImageSlicer.h
+++ Tools/MPR3DVTK94/vtkSplineDrivenImageSlicer.h
@@ -1,3 +1,25 @@
+// Copyright (c) 2010, Jérôme Velut
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT OWNER ``AS IS'' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE COPYRIGHT OWNER BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 //! \class vtkSplineDrivenImageSlicer
 //! \brief Reslicing of a volume along a path
 //!
@@ -27,67 +49,67 @@ class vtkImageReslice;
 class  vtkSplineDrivenImageSlicer : public vtkImageAlgorithm
 {
 public:
-   vtkTypeRevisionMacro(vtkSplineDrivenImageSlicer,vtkImageAlgorithm);
-   static vtkSplineDrivenImageSlicer* New();
+  vtkTypeMacro(vtkSplineDrivenImageSlicer,vtkImageAlgorithm);
+  static vtkSplineDrivenImageSlicer* New();
 
   //! Specify the path represented by a vtkPolyData wich contains PolyLines
   void SetPathConnection(int id, vtkAlgorithmOutput* algOutput);
   void SetPathConnection(vtkAlgorithmOutput* algOutput)
-    {
-      this->SetPathConnection(0, algOutput);
-    };
+  {
+    this->SetPathConnection(0, algOutput);
+  };
   vtkAlgorithmOutput* GetPathConnection( )
-                                 {return( this->GetInputConnection( 1, 0 ) );};
+  {return( this->GetInputConnection( 1, 0 ) );};
 
-   vtkSetVector2Macro( SliceExtent, vtkIdType );
-   vtkGetVector2Macro( SliceExtent, vtkIdType );
+  vtkSetVector2Macro( SliceExtent, int );
+  vtkGetVector2Macro( SliceExtent, int );
 
-   vtkSetVector2Macro( SliceSpacing, double );
-   vtkGetVector2Macro( SliceSpacing, double );
+  vtkSetVector2Macro( SliceSpacing, double );
+  vtkGetVector2Macro( SliceSpacing, double );
 
-   vtkSetMacro( SliceThickness, double );
-   vtkGetMacro( SliceThickness, double );
+  vtkSetMacro( SliceThickness, double );
+  vtkGetMacro( SliceThickness, double );
 
-   vtkSetMacro( OffsetPoint, vtkIdType );
-   vtkGetMacro( OffsetPoint, vtkIdType );
+  vtkSetMacro( OffsetPoint, vtkIdType );
+  vtkGetMacro( OffsetPoint, vtkIdType );
 
-   vtkSetMacro( OffsetLine, vtkIdType );
-   vtkGetMacro( OffsetLine, vtkIdType );
+  vtkSetMacro( OffsetLine, vtkIdType );
+  vtkGetMacro( OffsetLine, vtkIdType );
 
-   vtkSetMacro( ProbeInput, vtkIdType );
-   vtkGetMacro( ProbeInput, vtkIdType );
-   vtkBooleanMacro( ProbeInput, vtkIdType );
+  vtkSetMacro( ProbeInput, vtkIdType );
+  vtkGetMacro( ProbeInput, vtkIdType );
+  vtkBooleanMacro( ProbeInput, vtkIdType );
 
-   vtkSetMacro( Incidence, double );
-   vtkGetMacro( Incidence, double );
+  vtkSetMacro( Incidence, double );
+  vtkGetMacro( Incidence, double );
 
 
 protected:
-   vtkSplineDrivenImageSlicer();
-   ~vtkSplineDrivenImageSlicer();
+  vtkSplineDrivenImageSlicer();
+  ~vtkSplineDrivenImageSlicer();
 
-   virtual int RequestData(vtkInformation *, vtkInformationVector **, 
-	                                         vtkInformationVector *);
+  virtual int RequestData(vtkInformation *, vtkInformationVector **, 
+                          vtkInformationVector *) override;
 
-   virtual int FillInputPortInformation(int port, vtkInformation *info);
-   virtual int FillOutputPortInformation( int, vtkInformation*);
-   virtual int RequestInformation(vtkInformation*, vtkInformationVector**, 
-	                                                vtkInformationVector*);
+  virtual int FillInputPortInformation(int port, vtkInformation *info) override;
+  virtual int FillOutputPortInformation( int, vtkInformation*) override;
+  virtual int RequestInformation(vtkInformation*, vtkInformationVector**, 
+                                 vtkInformationVector*) override;
 private:
-   vtkSplineDrivenImageSlicer(const vtkSplineDrivenImageSlicer&);  // Not implemented.
-   void operator=(const vtkSplineDrivenImageSlicer&);  // Not implemented.
+  vtkSplineDrivenImageSlicer(const vtkSplineDrivenImageSlicer&) = delete;
+  void operator=(const vtkSplineDrivenImageSlicer&) = delete;
 
-   vtkFrenetSerretFrame* localFrenetFrames; //!< computes local tangent along path input
-   vtkImageReslice* reslicer; //!< Reslicers array
+  vtkFrenetSerretFrame* localFrenetFrames; //!< computes local tangent along path input
+  vtkImageReslice* reslicer; //!< Reslicers array
 
-   vtkIdType SliceExtent[2]; //!< Number of pixels nx, ny in the slice space around the center points
-   double SliceSpacing[2]; //!< Pixel size sx, sy of the output slice
-   double SliceThickness; //!< Slice thickness (useful for volumic reconstruction) 
-   double Incidence; //!< Rotation of the initial normal vector.
+  int     SliceExtent[2]; //!< Number of pixels nx, ny in the slice space around the center points
+  double SliceSpacing[2]; //!< Pixel size sx, sy of the output slice
+  double SliceThickness; //!< Slice thickness (useful for volumic reconstruction) 
+  double Incidence; //!< Rotation of the initial normal vector.
    
-   vtkIdType OffsetPoint; //!< Id of the point where the reslicer proceed
-   vtkIdType OffsetLine; //!< Id of the line cell where to get the reslice center
-   vtkIdType ProbeInput; //!< If true, the output plane (2nd output probes the input image)
+  vtkIdType OffsetPoint; //!< Id of the point where the reslicer proceed
+  vtkIdType OffsetLine; //!< Id of the line cell where to get the reslice center
+  vtkIdType ProbeInput; //!< If true, the output plane (2nd output probes the input image)
 };
 
 #endif //__vtkSplineDrivenImageSlicer_h__
