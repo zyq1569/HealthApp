13de61dfd493796fe6b0f0bb44635e862ad5f15e
zyq1569 <zyq1569@126.com>
2025-09-16 11:49:43 +0800

    fine-tune

--- Tools/Test/CPRVTK/QtVTKRenderWindows.cxx
+++ Tools/Test/CPRVTK/QtVTKRenderWindows.cxx
@@ -611,7 +611,9 @@ void QtVTKRenderWindows::initDir()
 void QtVTKRenderWindows::initMPR()
 {
     vtkNew<vtkMetaImageReader> reader;
-    QString dir = "D:/Test_DICOM/INCISIX-Dental_MHD/3DMetaData.mhd";
+    //QString dir = "D:/Test_DICOM/INCISIX-Dental_MHD/3DMetaData.mhd";
+    QString dir = QCoreApplication::applicationDirPath() + "/3DMetaData.mhd";
+    ui->m_dcmDIR->setPlainText(dir);
     //dir = "D:/TEMP/SZDL/test-data/MPR_0408.mhd";
     std::string stddir = qPrintable(dir);
     reader->SetFileName(stddir.c_str());
@@ -787,6 +789,8 @@ QtVTKRenderWindows::QtVTKRenderWindows(int vtkNotUsed(argc), char* argv[])
     connect(this->ui->m_pbCPR, SIGNAL(pressed()), this, SLOT(StarCPR()));
 
     connect(this->ui->m_Dir, SIGNAL(pressed()), this, SLOT(initDir()));
+    QString dir = QCoreApplication::applicationDirPath() + "/3DMetaData.mhd";
+    ui->m_dcmDIR->setPlainText(dir);
 };
 
 void QtVTKRenderWindows::StarCPR()
